2f90a08bed79a0c4129236717d93d0293ec98d6a
itsAnkitAkm <itsamkankit@gmail.com>
2024-05-20 20:17:22 +0530

    Add getCurrentUser Route

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {registerUser, loginUser, logoutUser, refreshAccessToken, changePassword, upadateAccountDetails, updateUserAvatar} from "../controllers/user.controller.js";
+import {registerUser, loginUser, logoutUser, refreshAccessToken, changePassword, upadateAccountDetails, updateUserAvatar, getCurrentUser} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -24,5 +24,7 @@ router.route("/change-password").post(verifyJWT, changePassword)
 router.route("/update-account").patch(verifyJWT,upadateAccountDetails)
 
 router.route("/avatar").patch (verifyJWT,upload.single("avatar"), updateUserAvatar)
+
+router.route("/current-user").get(verifyJWT,getCurrentUser)
 
 export default router;
